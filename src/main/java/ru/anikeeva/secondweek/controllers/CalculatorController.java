5c1bc8add904a9a132357d9d7bcde9993fc74b9d
Okta013 <sindorayka@yandex.ru>
2024-07-14 22:04:31 +0400

    Calculator page

---
+++ src/main/java/ru/anikeeva/secondweek/controllers/CalculatorController.java
@@ -0,0 +1,43 @@
+package ru.anikeeva.secondweek.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.anikeeva.secondweek.models.Calc;
+
+@Controller
+@RequestMapping("/")
+public class CalculatorController {
+
+    @GetMapping("/calculator")
+    public String showCalculator(Model model) {
+        model.addAttribute("calc", new Calc());
+        return "calculator";
+    }
+
+    @PostMapping()
+    public String calculate(@ModelAttribute Calc form){
+        double result;
+
+        switch (form.getAction()) {
+            case "multiplication":
+                result = form.getNumberOne() * form.getNumberTwo();
+                break;
+            case "division":
+                result = form.getNumberOne() / form.getNumberTwo();
+                break;
+            case "subtraction":
+                result = form.getNumberOne() - form.getNumberTwo();
+                break;
+            case "addition":
+                result = form.getNumberOne() + form.getNumberTwo();
+                break;
+            default:
+                result = 0.0;
+                break;
+        }
+
+        form.setResult(result);
+        return "redirect:/calculator";
+    }
+}
---
+++ src/main/java/ru/anikeeva/secondweek/models/Calc.java
@@ -0,0 +1,40 @@
+package ru.anikeeva.secondweek.models;
+
+public class Calc {
+    private double numberOne;
+    private double numberTwo;
+    private String action;
+    private double result;
+
+    public double getNumberOne() {
+        return numberOne;
+    }
+
+    public void setNumberOne(double numberOne) {
+        this.numberOne = numberOne;
+    }
+
+    public double getNumberTwo() {
+        return numberTwo;
+    }
+
+    public void setNumberTwo(double numberTwo) {
+        this.numberTwo = numberTwo;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+}
