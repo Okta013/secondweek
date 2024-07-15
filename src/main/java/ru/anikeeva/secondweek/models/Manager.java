7f984e8125515d26ba57f0a6cb191f699c3290ee
okta013 <sindorayka@yandex.ru>
2024-07-15 16:03:19 +0300

    Add class ManagerDAO

--- src/main/java/ru/anikeeva/secondweek/models/Manager.java
+++ src/main/java/ru/anikeeva/secondweek/models/Manager.java
@@ -1,8 +1,10 @@
 package ru.anikeeva.secondweek.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Manager {
+    private int id;
     private String name;
     private String surname;
     private int age;
@@ -10,4 +12,59 @@ public class Manager {
     private List<Employee> employees;
 
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getExperience() {
+        return experience;
+    }
+
+    public void setExperience(double experience) {
+        this.experience = experience;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Manager(int id, String name, String surname, int age, double experience) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.experience = experience;
+    }
 }
